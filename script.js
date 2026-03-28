2dc803a286922c38cf43e6332e4e85f39d934d33
QuocDatRio <laiquocdat101204@gmail.com>
2026-03-28 23:40:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('Portfolio đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình');
+
+    const projects = document.querySelectorAll('.project');
+
+    projects.forEach(p => {
+        p.addEventListener('click', () => {
+            alert('Bạn vừa chọn: ' + p.innerText);
+        });
+    });
+
+});
